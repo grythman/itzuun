1daba44955036111e1830817b0e38a35c85ff44d
baatarsurentuguldur450 <baatarsurentuguldur450@gmail.com>
2026-03-27 08:50:53 +0000

    Fix: Isolate escrow_service by commenting out qpay imports

--- backend/apps/payments/services/__init__.py
+++ backend/apps/payments/services/__init__.py
@@ -12,21 +12,21 @@ from .escrow_service import (
     resolve_dispute,
     submit_result,
 )
-from .qpay_service import authenticate, create_invoice, verify_webhook
+# from .qpay_service import authenticate, create_invoice, verify_webhook
 
 __all__ = [
     "_build_hash_chain",
     "approve_escrow",
-    "authenticate",
+    # "authenticate",
     "calculate_commission",
     "confirm_completion",
     "create_dispute",
-    "create_invoice",
+    # "create_invoice",
     "deposit_to_escrow",
     "expire_stale_pending_payments",
     "mark_payment_failed",
     "mark_payment_paid_and_hold_escrow",
     "resolve_dispute",
     "submit_result",
-    "verify_webhook",
+    # "verify_webhook",
 ]
